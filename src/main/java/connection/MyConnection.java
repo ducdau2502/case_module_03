c9bc159fe056ebce6e3a7314b0a24c9216ff244b
TienNguyen0612 <sales08.taybactravel@gmail.com>
2022-02-09 10:01:11 +0700

    add linh tinh 2

--- src/main/java/connection/MyConnection.java
+++ src/main/java/connection/MyConnection.java
@@ -16,6 +16,7 @@ public class MyConnection {
             // Đổi class hiện tại về quản lý như 1 drive
             // Đổi class hiện tại về quản lý như 1 drive
             // Đổi class hiện tại về quản lý như 1 drive
+            // Đổi class hiện tại về quản lý như 1 drive
             Class.forName("com.mysql.jdbc.Driver");
 
             //Quản lý connection vs driver manager
