368ccb6640090204518a9e76d600a38e64e9bd73
ducdau <dauducdh2cm1@gmail.com>
2022-02-09 13:37:58 +0700

    test buoi chieu lan 1

--- src/main/java/connection/MyConnection.java
+++ src/main/java/connection/MyConnection.java
@@ -5,7 +5,7 @@ import java.sql.DriverManager;
 import java.sql.SQLException;
 
 public class MyConnection {
-    private String jdbcURL = "jdbc:mysql://localhost:3306/case_module_03?useSSL=false";
+    private String jdbcURL = "jdbc:mysql://localhost:3306/case_module_03_DucDepZai?useSSL=false";
     private String jdbcUsername = "root";
     private String jdbcPassword = "123456";
 
