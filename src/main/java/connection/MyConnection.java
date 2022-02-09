b719289b07d413b3a4774525c8910cab8aef24ba
ducdau <dauducdh2cm1@gmail.com>
2022-02-09 14:48:59 +0700

    t

--- src/main/java/connection/MyConnection.java
+++ src/main/java/connection/MyConnection.java
@@ -9,7 +9,7 @@ public class MyConnection {
     private String jdbcURL1 = "jdbc:mysql://localhost:3306/case_module_03_C1021H1?useSSL=false";
     private String jdbcUsername = "root";
     private String jdbcPassword = "123456";
-    private String DucDepzai = "vl";
+    private String DucDepzai = "vờ lờ cờ";
 
     public Connection getConnection() {
         Connection connection = null;
