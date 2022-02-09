3da7b41d28b60b35d065f296dca6bc5f3318767e
ducdau <dauducdh2cm1@gmail.com>
2022-02-09 10:24:31 +0700

    fix bug

--- src/main/java/connection/MyConnection.java
+++ src/main/java/connection/MyConnection.java
@@ -13,24 +13,8 @@ public class MyConnection {
         Connection connection = null;
         try {
             // Đổi class hiện tại về quản lý như 1 drive
-            // Đổi class hiện tại về quản lý như 1 drive
-            // Đổi class hiện tại về quản lý như 1 drive
-            // Đổi class hiện tại về quản lý như 1 drive
-            // Đổi class hiện tại về quản lý như 1 drive
-            // Đổi class hiện tại về quản lý như 1 drive
-            // Đổi class hiện tại về quản lý như 1 drive
             Class.forName("com.mysql.jdbc.Driver");
 
-            //Quản lý connection vs driver manager
-            //Quản lý connection vs driver manager
-            //Quản lý connection vs driver manager
-            //Quản lý connection vs driver manager
-            //Quản lý connection vs driver manager
-            //Quản lý connection vs driver manager
-            //Quản lý connection vs driver manager
-            //Quản lý connection vs driver manager
-            //Quản lý connection vs driver manager
-            //Quản lý connection vs driver manager
             //Quản lý connection vs driver manager
             connection = DriverManager.getConnection(jdbcURL, jdbcUsername, jdbcPassword);
         } catch (SQLException | ClassNotFoundException e) {
